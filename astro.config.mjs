7cdd65aec94ed0c040ddbe49ada08442a157119c
Jéssica de Aquino <corayalina@gmail.com>
2025-08-11 17:29:26 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
 import partytown from '@astrojs/partytown';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), partytown()]
+  integrations: [react(), partytown()],
+  output: 'server',
+  adapter: vercel({
+    imageService: true,
+  }),
 });
